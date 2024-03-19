f9db96c2b6ceeacdbdfb9fdc5d5a94be866012a7
Corbin Fanning <corbin.hfanning@tamu.edu>
2024-03-19 16:19:38 -0500

    Initial Commit

---
+++ ToDoListMar19/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoListMar19
+//
+//  Created by Fanning, Corbin on 3/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoListMar19/ToDoListMar19App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListMar19App.swift
+//  ToDoListMar19
+//
+//  Created by Fanning, Corbin on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListMar19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
